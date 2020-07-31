d4737a75a8f9d91393c41f900c5913f5efbb34f8
Guilherme Santos <santosgui678@gmail.com>
2020-07-30 22:14:47 -0300

    Adicionando e configurando backend para o Heroku

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -86,7 +86,7 @@ function FormField({
 
   return (
     <FormFieldWrapper>
-      
+
       <Label htmlFor={fieldId}>
 
         <Input
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
@@ -25,6 +25,42 @@ function CadastroCategoria() {
     const { name, value } = e.target;
     setValue(name, value);
   }
+
+  useEffect(() => {
+    console.log('Mudou');
+    const URL = 'http://localhost:8080/categorias';
+    fetch(URL)
+      .then(async (response) => {
+        const resposta = await response.json();
+        setCategorias([
+          ...resposta,
+        ]);
+      });
+
+    // setTimeout(() => {
+    //   setCategorias([
+    //     ...categorias,
+    //     {
+    //       id: 1,
+    //       nome: 'Heavy Metal',
+    //       descricao: 'Gênero heavy',
+    //       cor: '#6bd1ff',
+    //     },
+    //     {
+    //       id: 2,
+    //       nome: 'Indie',
+    //       descricao: 'Gênero indie',
+    //       cor: '#d16bff',
+    //     },
+    //     {
+    //       id: 3,
+    //       nome: 'Punk Rock',
+    //       descricao: 'Gênero punk',
+    //       cor: '#ffd16c',
+    //     },
+    //   ]);
+    // }, 3 * 1000);
+  }, []);
 
   return (
     <PageDefault>
@@ -73,6 +109,12 @@ function CadastroCategoria() {
           Cadastrar
         </Button>
       </form>
+
+      {categorias.length === 0 && (
+      <div>
+        Loading ...
+      </div>
+      )}
 
       <ul>
         {categorias.map((categoria) => (
